f968d3711468feee08db7d99450bc3bcada5c831
Miguel Pafume <leugim.pafume@gmail.com>
2026-04-28 15:16:18 -0300

    add image path fallbacks and CharacterUiPath helper

--- nadaAquiModCode/Extensions/StringExtensions.cs
+++ nadaAquiModCode/Extensions/StringExtensions.cs
@@ -1,3 +1,5 @@
+using Godot;
+
 namespace nadaAquiMod.nadaAquiModCode.Extensions;
 
 //Mostly utilities to get asset paths.
@@ -7,33 +9,63 @@ public static class StringExtensions
     {
         return Path.Join(MainFile.ResPath, "images", path);
     }
-    
+
     public static string CardImagePath(this string path)
     {
-        return Path.Join(MainFile.ResPath, "images", "card_portraits", path);
+        path = Path.Join(MainFile.ResPath, "images", "card_portraits", path);
+        if (ResourceLoader.Exists(path)) return path;
+        
+        MainFile.Logger.Info("Could not find card image path: " + path);
+        return Path.Join(MainFile.ResPath, "images", "card_portraits", "card.png");
     }
+
     public static string BigCardImagePath(this string path)
     {
-        return Path.Join(MainFile.ResPath, "images", "card_portraits", "big", path);
+        path = Path.Join(MainFile.ResPath, "images", "card_portraits", "big", path);
+        if (ResourceLoader.Exists(path)) return path;
+        
+        MainFile.Logger.Info("Could not find big card image path: " + path);
+        return Path.Join(MainFile.ResPath, "images", "card_portraits", "big", "card.png");
     }
 
     public static string PowerImagePath(this string path)
     {
-        return Path.Join(MainFile.ResPath, "images", "powers", path);
+        path = Path.Join(MainFile.ResPath, "images", "powers", path);
+        if (ResourceLoader.Exists(path)) return path;
+        
+        MainFile.Logger.Info("Could not find power image path: " + path);
+        return Path.Join(MainFile.ResPath, "images", "powers", "power.png");
     }
 
     public static string BigPowerImagePath(this string path)
     {
-        return Path.Join(MainFile.ResPath, "images", "powers", "big", path);
+        path = Path.Join(MainFile.ResPath, "images", "powers", "big", path);
+        if (ResourceLoader.Exists(path)) return path;
+        
+        MainFile.Logger.Info("Could not find big power image path: " + path);
+        return Path.Join(MainFile.ResPath, "images", "powers", "big", "power.png");
     }
 
     public static string RelicImagePath(this string path)
     {
-        return Path.Join(MainFile.ResPath, "images", "relics", path);
+        path = Path.Join(MainFile.ResPath, "images", "relics", path);
+        if (ResourceLoader.Exists(path)) return path;
+        
+        MainFile.Logger.Info("Could not find relic image path: " + path);
+        return Path.Join(MainFile.ResPath, "images", "relics", "relic.png");
     }
 
     public static string BigRelicImagePath(this string path)
     {
-        return Path.Join(MainFile.ResPath, "images", "relics", "big", path);
+        path = Path.Join(MainFile.ResPath, "images", "relics", "big", path);
+        if (ResourceLoader.Exists(path)) return path;
+        
+        MainFile.Logger.Info("Could not find big relic image path: " + path);
+        return Path.Join(MainFile.ResPath, "images", "relics", "big", "relic.png");
+    }
+
+    public static string CharacterUiPath(this string path)
+    {
+        return Path.Join(MainFile.ResPath, "images", "charui", path);
     }
 }
